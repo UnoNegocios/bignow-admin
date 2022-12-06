ef735b04b8e3722d327a16f140f9a5a09816e1fb
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-05 19:53:38 -0600

    service worker

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@
       <app v-if="token!=null"></app>
       <login v-else></login>
     </v-main>
+    <v-snackbar top :value="updateExists" color="#e74919" timeout="999999999" style="margin-top:-72px;" content-class="py-1"><!--updateExists-->
+      <span style="font-size:16px;">Hay una actualización disponible!</span>
+      <v-btn class="ml-4 elevation-0" small style="margin-top:-4px;" color="white" light @click="refreshApp">
+        Actualizar
+      </v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import update from './mixins/update'
 import axios from "axios"
 import App from "../src/views/App"
 import Login from "../src/views/Login"
